d280d622ea9ea594d0f98c67000268342b9f5974
neuropassenger <turgenoid@gmail.com>
2023-10-14 15:21:44 +0400

    gutenberg block created

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '54c923b03ce4f3d19862');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c0c010702d6f062de5b0');
