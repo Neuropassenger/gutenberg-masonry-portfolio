4adeb9bc7939652c5dfbf8c66d7922e443d9dd9a
neuropassenger <turgenoid@gmail.com>
2023-10-19 18:37:06 +0400

    Add title and overlay for Masonry Items

--- src/components/MasonryPost.js
+++ src/components/MasonryPost.js
@@ -2,6 +2,7 @@ const MasonryPost = (props) => {
     return (
         <div className="bws_masonry-post">
             <a href={props.postData.link}>
+                <h2>{props.postData.title.rendered}</h2>
                 <img src={props.postData._embedded["wp:featuredmedia"][0].source_url} alt={props.postData.title.rendered} />
             </a>
         </div>
