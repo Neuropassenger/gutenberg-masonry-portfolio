caae388db8ec7745e7a1d3b78d412a23fabf36ab
neuropassenger <turgenoid@gmail.com>
2023-10-21 14:14:29 +0400

    Build

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components'), 'version' => 'e8f105cb87e7899cc523');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components'), 'version' => 'ce0a04e6e9538b9a77a3');
