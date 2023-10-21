1d589b5aea63b2b9cef8b321004aad54f0c25fdb
neuropassenger <turgenoid@gmail.com>
2023-10-21 13:52:24 +0400

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f551d1fc266b15e50abc');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '62b9234c80284adc8d8f');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '64d3bec7cd92c1ff7aab');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch'), 'version' => '5f7b127cff231e055d87');
