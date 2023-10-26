481c9305dd10dca6cae31bf7ee7fb231d6477508
neuropassenger <turgenoid@gmail.com>
2023-10-26 21:37:29 +0400

    Display html entities correctly

--- src/components/MasonryPost.js
+++ src/components/MasonryPost.js
@@ -1,12 +1,14 @@
+import he from 'he';
+
 const MasonryPost = (props) => {
     return (
         <div className="bws_masonry-post">
             <a href={props.postData.link}>
-                <h2>{props.postData.title.rendered}</h2>
+                <h2>{he.decode(props.postData.title.rendered)}</h2>
                 <img 
                     loading="lazy" 
                     src={props.postData._embedded["wp:featuredmedia"][0].source_url} 
-                    alt={props.postData.title.rendered} 
+                    alt={he.decode(props.postData.title.rendered)} 
                 />
             </a>
         </div>
